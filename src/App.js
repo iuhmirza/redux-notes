059eb11305ab7899f3cf0db62ad3de0147e6b844
Ibtisam Mirza <iuhmirza@gmail.com>
2022-11-22 12:53:55 +0000

    Add unit tests for reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
+import noteReducer from "./reducers/noteReducer"
 import { createStore } from "redux"
-
-const noteReducer = (state = [], action) => {
-  if(action.type === 'NEW_NOTE') {
-    state.push(action.data)
-    return state
-  }
-  return state
-}
 
 const store = createStore(noteReducer)
 
