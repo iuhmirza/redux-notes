0df14a409f6ae8bec8397635f33d95c3f896b7de
Ibtisam Mirza <iuhmirza@gmail.com>
2022-11-24 13:39:36 +0000

    Add Redux Toolkit and refactor reducers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App'
 
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
@@ -16,14 +19,9 @@ const reducer = combineReducers({
   filter: filterReducer
 })
 
-const store = createStore(reducer)
+const store =  configureStore({reducer})
 
 console.log(store.getState())
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
