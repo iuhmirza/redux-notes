9b0f6d507063c1b7be46ba22e2aa25b5c82b9f41
Ibtisam Mirza <iuhmirza@gmail.com>
2022-11-27 16:42:49 +0000

    Use connect to share redux store

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,4 +1,5 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { connect } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { toggleImportanceOf } from '../reducers/noteReducer'
 
 const Note = ({ note, handleClick }) => {
@@ -10,20 +11,14 @@ const Note = ({ note, handleClick }) => {
   )
 }
 
-const Notes = () => {
+const Notes = (props) => {
   const dispatch = useDispatch()
-  const notes = useSelector(({ filter, notes }) => {
-    if ( filter === 'ALL' ) {
-      return notes
-    }
-    return filter  === 'IMPORTANT' 
-      ? notes.filter(note => note.important)
-      : notes.filter(note => !note.important)
-  })
+
+  
   
   return(
     <ul>
-      {notes.map(note =>
+      {props.notes.map(note =>
         <Note
           key={note.id}
           note={note}
@@ -36,4 +31,14 @@ const Notes = () => {
   )
 }
 
-export default Notes
+const mapStateToProps = (state) => {
+  if ( state.filter === 'ALL' ) {
+    return { state: state.notes}
+  }
+  return { notes: state.filter  === 'IMPORTANT' 
+    ? state.notes.filter(note => note.important)
+    : state.notes.filter(note => !note.important)}
+}
+
+const ConnectedNotes = connect(mapStateToProps)(Notes)
+export default ConnectedNotes
